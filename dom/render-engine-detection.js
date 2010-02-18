2f28bb6e238de1c1648d200d357e1a5e50ea356d
petsel <peter.seliger@gmail.com>
2010-02-18 15:43:02 +0100

    simple DOM-Scripting helper that detects property support of an [HTMLElement]-/[Node]- objects [style] property.
    Signed-off-by: petsel <peter.seliger@gmail.com>

---
+++ dom/render-engine-detection.js
@@ -0,0 +1,30 @@
+﻿/*
+
+(function () { // some APPLICATION module/context */
+
+
+//var sh/*global*/ = ((this && (this.window === this) && /*this.*/window) || this), // "scripting host" or "global object"
+
+
+	RenderEngine = (function (/*styleOfDocElm*/) { // local helper namespace holding css feature detection results.
+
+	//var styleOfDocElm = ((sh.document && (sh.document.documentElement || ((sh.document.getElementsByTagName && sh.document.getElementsByTagName("html")[0]) || {})).style) || {}),
+		var styleOfDocElm = ((document && (document.documentElement || ((document.getElementsByTagName && document.getElementsByTagName("html")[0]) || {})).style) || {}),
+
+		getIsSupported = (function (propertyName) {
+			return (typeof styleOfDocElm[propertyName] == "string");
+		});
+		return {
+
+			isGECKO   : getIsSupported("MozOpacity"),
+			isKHTML   : (getIsSupported("KhtmlOpacity") && !getIsSupported("WebkitOpacity")),
+			isWEBKIT  : getIsSupported("WebkitOpacity"),
+			isPRESTO  : getIsSupported("opPhonemes"),
+			isTRIDENT : getIsSupported("behavior"),
+
+			isWebkitTransform : getIsSupported("WebkitTransform")
+		};
+	})(/*(document.documentElement || document.getElementsByTagName("html")[0]).style*/);
+/*
+
+})();*/
