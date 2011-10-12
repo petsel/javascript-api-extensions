09ace58097f87a48e7990fa1c8e37aa1eb579494
Peter Seliger <peter.seliger@googlemail.com>
2011-10-12 16:12:55 +0200

    methods wording changed from [between] to [inBetween] in order to keep it as much precisely as with [compare] vs [compareTo].
    Signed-off-by: Peter Seliger <peter.seliger@googlemail.com>

--- core/Comparable/ComparableTrait.js
+++ core/Comparable/ComparableTrait.js
@@ -67,15 +67,15 @@
 
         return customCompareTypes(this, obj, customValueOf);
       });
-      this.between = (function (objK, objM/*objects [this] does compare to*/, customValueOf/*during object comparison an optional custom [valueOf] method will be called for each object rigth before comparing them to one another*/) {
+      this.inBetween = (function (objK, objM/*objects [this] does compare to*/, customValueOf/*during object comparison an optional custom [valueOf] method will be called for each object rigth before comparing them to one another*/) {
 
-        var isBetween = customCompareTypes(objK, objM, customValueOf); // -1 || 0 || 1;
-        if (isBetween < 0) {
-          isBetween = (customCompareTypes(this, objK, customValueOf) > 0) && (customCompareTypes(this, objM, customValueOf) < 0);
-        } else if (isBetween > 0) {
-          isBetween = (customCompareTypes(this, objM, customValueOf) > 0) && (customCompareTypes(this, objK, customValueOf) < 0);
+        var isInBetween = customCompareTypes(objK, objM, customValueOf); // -1 || 0 || 1;
+        if (isInBetween < 0) {
+          isInBetween = (customCompareTypes(this, objK, customValueOf) > 0) && (customCompareTypes(this, objM, customValueOf) < 0);
+        } else if (isInBetween > 0) {
+          isInBetween = (customCompareTypes(this, objM, customValueOf) > 0) && (customCompareTypes(this, objK, customValueOf) < 0);
         }
-        return !!isBetween;
+        return !!isInBetween;
       });
     });
   })(defaultCompareTypes);
@@ -202,9 +202,9 @@ log("y.compareTo(z, oddityValueOf) : " + y.compareTo(z, oddityValueOf)); // 0
 
 
 
-log("2..between(1, 3) ? " + 2..between(1, 3)); // true
-log("2..between(2, 3) ? " + 2..between(2, 3)); // false
-log("2..between(1, 2) ? " + 2..between(1, 2)); // false
+log("2..inBetween(1, 3) ? " + 2..inBetween(1, 3)); // true
+log("2..inBetween(2, 3) ? " + 2..inBetween(2, 3)); // false
+log("2..inBetween(1, 2) ? " + 2..inBetween(1, 2)); // false
 
 
 
@@ -222,12 +222,12 @@ ComparableTrait.call(String.prototype);
 
 log("ComparableTrait.call(String.prototype);");
 
-log("\"cat\".between(\"ant\", \"dog\") ? " + "cat".between("ant", "dog")); // true
-log("\"gnu\".between(\"ant\", \"dog\") ? " + "gnu".between("ant", "dog")); // false
+log("\"cat\".inBetween(\"ant\", \"dog\") ? " + "cat".inBetween("ant", "dog")); // true
+log("\"gnu\".inBetween(\"ant\", \"dog\") ? " + "gnu".inBetween("ant", "dog")); // false
 
-log("\"a\".between(\"a\", \"d\") ? " + "a".between("a", "d"));    // false
-log("\"aa\".between(\"a\", \"d\") ? " + "aa".between("a", "d"));  // true
-log("\"b\".between(\"a\", \"d\") ? " + "b".between("a", "d"));    // true
+log("\"a\".inBetween(\"a\", \"d\") ? " + "a".inBetween("a", "d"));    // false
+log("\"aa\".inBetween(\"a\", \"d\") ? " + "aa".inBetween("a", "d"));  // true
+log("\"b\".inBetween(\"a\", \"d\") ? " + "b".inBetween("a", "d"));    // true
 
 
 
@@ -237,11 +237,11 @@ log("\"b\".between(\"a\", \"d\") ? " + "b".between("a", "d"));    // true
   [http://closure-compiler.appspot.com/home]
 
 
-- Whitespace only - 1.401 byte
-(function(ns){var global=this;if(typeof(ns||global).ComparableTrait=="function")return;var defaultValueOf=function(proto_value_of){return function(type){return proto_value_of.call(type).valueOf()}}(global.Object.prototype.valueOf),defaultCompareTypes=function(default_value_of){return function(typeA,typeB,customValueOf){customValueOf=typeof customValueOf=="function"&&customValueOf||default_value_of;var valueA,valueB;return(valueA=customValueOf(typeA))>(valueB=customValueOf(typeB))&&1||valueA<valueB&&-1||0}}(defaultValueOf),ComparableTrait=function(default_compare_types){return function(customCompareTypes){customCompareTypes=typeof customCompareTypes=="function"&&customCompareTypes||default_compare_types;this.compareTo=function(obj,customValueOf){return customCompareTypes(this,obj,customValueOf)};this.between=function(objK,objM,customValueOf){var isBetween=customCompareTypes(objK,objM,customValueOf);if(isBetween<0)isBetween=customCompareTypes(this,objK,customValueOf)>0&&customCompareTypes(this,objM,customValueOf)<0;else if(isBetween>0)isBetween=customCompareTypes(this,objM,customValueOf)>0&&customCompareTypes(this,objK,customValueOf)<0;return!!isBetween}}}(defaultCompareTypes);(ns||global).ComparableTrait=ComparableTrait;ComparableTrait=defaultCompareTypes=defaultValueOf=global=null;delete ComparableTrait;delete defaultCompareTypes;delete defaultValueOf;delete global}).call(null);
+- Whitespace only - 1.415 byte
+(function(ns){var global=this;if(typeof(ns||global).ComparableTrait=="function")return;var defaultValueOf=function(proto_value_of){return function(type){return proto_value_of.call(type).valueOf()}}(global.Object.prototype.valueOf),defaultCompareTypes=function(default_value_of){return function(typeA,typeB,customValueOf){customValueOf=typeof customValueOf=="function"&&customValueOf||default_value_of;var valueA,valueB;return(valueA=customValueOf(typeA))>(valueB=customValueOf(typeB))&&1||valueA<valueB&&-1||0}}(defaultValueOf),ComparableTrait=function(default_compare_types){return function(customCompareTypes){customCompareTypes=typeof customCompareTypes=="function"&&customCompareTypes||default_compare_types;this.compareTo=function(obj,customValueOf){return customCompareTypes(this,obj,customValueOf)};this.inBetween=function(objK,objM,customValueOf){var isInBetween=customCompareTypes(objK,objM,customValueOf);if(isInBetween<0)isInBetween=customCompareTypes(this,objK,customValueOf)>0&&customCompareTypes(this,objM,customValueOf)<0;else if(isInBetween>0)isInBetween=customCompareTypes(this,objM,customValueOf)>0&&customCompareTypes(this,objK,customValueOf)<0;return!!isInBetween}}}(defaultCompareTypes);(ns||global).ComparableTrait=ComparableTrait;ComparableTrait=defaultCompareTypes=defaultValueOf=global=null;delete ComparableTrait;delete defaultCompareTypes;delete defaultValueOf;delete global}).call(null);
 
-- Simple          -   613 byte
-(function(i){var a=this;if(typeof(i||a).ComparableTrait!="function"){var f=function(a){return function(b){return a.call(b).valueOf()}}(a.Object.prototype.valueOf),g=function(a){return function(b,j,c){var c=typeof c=="function"&&c||a,d,e;return(d=c(b))>(e=c(j))&&1||d<e&&-1||0}}(f),h=function(a){return function(b){b=typeof b=="function"&&b||a;this.compareTo=function(a,c){return b(this,a,c)};this.between=function(a,c,d){var e=b(a,c,d);e<0?e=b(this,a,d)>0&&b(this,c,d)<0:e>0&&(e=b(this,c,d)>0&&b(this,a,d)<0);return!!e}}}(g);(i||a).ComparableTrait=h;h=g=f=a=null;delete h;delete g;delete f;delete a}}).call(null);
+- Simple          -   615 byte
+(function(i){var a=this;if(typeof(i||a).ComparableTrait!="function"){var f=function(a){return function(b){return a.call(b).valueOf()}}(a.Object.prototype.valueOf),g=function(a){return function(b,j,c){var c=typeof c=="function"&&c||a,d,e;return(d=c(b))>(e=c(j))&&1||d<e&&-1||0}}(f),h=function(a){return function(b){b=typeof b=="function"&&b||a;this.compareTo=function(a,c){return b(this,a,c)};this.inBetween=function(a,c,d){var e=b(a,c,d);e<0?e=b(this,a,d)>0&&b(this,c,d)<0:e>0&&(e=b(this,c,d)>0&&b(this,a,d)<0);return!!e}}}(g);(i||a).ComparableTrait=h;h=g=f=a=null;delete h;delete g;delete f;delete a}}).call(null);
 
 
 */
--- core/Comparable/ComparableTrait.module.RequireJS.js
+++ core/Comparable/ComparableTrait.module.RequireJS.js
@@ -62,15 +62,15 @@ define(function () {
 
           return customCompareTypes(this, obj, customValueOf);
         });
-        this.between = (function (objK, objM/*objects [this] does compare to*/, customValueOf/*during object comparison an optional custom [valueOf] method will be called for each object rigth before comparing them to one another*/) {
+        this.inBetween = (function (objK, objM/*objects [this] does compare to*/, customValueOf/*during object comparison an optional custom [valueOf] method will be called for each object rigth before comparing them to one another*/) {
 
-          var isBetween = customCompareTypes(objK, objM, customValueOf); // -1 || 0 || 1;
-          if (isBetween < 0) {
-            isBetween = (customCompareTypes(this, objK, customValueOf) > 0) && (customCompareTypes(this, objM, customValueOf) < 0);
-          } else if (isBetween > 0) {
-            isBetween = (customCompareTypes(this, objM, customValueOf) > 0) && (customCompareTypes(this, objK, customValueOf) < 0);
+          var isInBetween = customCompareTypes(objK, objM, customValueOf); // -1 || 0 || 1;
+          if (isInBetween < 0) {
+            isInBetween = (customCompareTypes(this, objK, customValueOf) > 0) && (customCompareTypes(this, objM, customValueOf) < 0);
+          } else if (isInBetween > 0) {
+            isInBetween = (customCompareTypes(this, objM, customValueOf) > 0) && (customCompareTypes(this, objK, customValueOf) < 0);
           }
-          return !!isBetween;
+          return !!isInBetween;
         });
       });
     })(defaultCompareTypes);
@@ -92,11 +92,11 @@ define(function () {
   [http://closure-compiler.appspot.com/home]
 
 
-- Whitespace only - 1.302 byte
-define(function(){return function(){var global=this,defaultValueOf=function(proto_value_of){return function(type){return proto_value_of.call(type).valueOf()}}(global.Object.prototype.valueOf),defaultCompareTypes=function(default_value_of){return function(typeA,typeB,customValueOf){customValueOf=typeof customValueOf=="function"&&customValueOf||default_value_of;var valueA,valueB;return(valueA=customValueOf(typeA))>(valueB=customValueOf(typeB))&&1||valueA<valueB&&-1||0}}(defaultValueOf),ComparableTrait=function(default_compare_types){return function(customCompareTypes){customCompareTypes=typeof customCompareTypes=="function"&&customCompareTypes||default_compare_types;this.compareTo=function(obj,customValueOf){return customCompareTypes(this,obj,customValueOf)};this.between=function(objK,objM,customValueOf){var isBetween=customCompareTypes(objK,objM,customValueOf);if(isBetween<0)isBetween=customCompareTypes(this,objK,customValueOf)>0&&customCompareTypes(this,objM,customValueOf)<0;else if(isBetween>0)isBetween=customCompareTypes(this,objM,customValueOf)>0&&customCompareTypes(this,objK,customValueOf)<0;return!!isBetween}}}(defaultCompareTypes);defaultCompareTypes=defaultValueOf=global=null;delete defaultCompareTypes;delete defaultValueOf;delete global;return ComparableTrait}.call(null)});
+- Whitespace only - 1.316 byte
+define(function(){return function(){var global=this,defaultValueOf=function(proto_value_of){return function(type){return proto_value_of.call(type).valueOf()}}(global.Object.prototype.valueOf),defaultCompareTypes=function(default_value_of){return function(typeA,typeB,customValueOf){customValueOf=typeof customValueOf=="function"&&customValueOf||default_value_of;var valueA,valueB;return(valueA=customValueOf(typeA))>(valueB=customValueOf(typeB))&&1||valueA<valueB&&-1||0}}(defaultValueOf),ComparableTrait=function(default_compare_types){return function(customCompareTypes){customCompareTypes=typeof customCompareTypes=="function"&&customCompareTypes||default_compare_types;this.compareTo=function(obj,customValueOf){return customCompareTypes(this,obj,customValueOf)};this.inBetween=function(objK,objM,customValueOf){var isInBetween=customCompareTypes(objK,objM,customValueOf);if(isInBetween<0)isInBetween=customCompareTypes(this,objK,customValueOf)>0&&customCompareTypes(this,objM,customValueOf)<0;else if(isInBetween>0)isInBetween=customCompareTypes(this,objM,customValueOf)>0&&customCompareTypes(this,objK,customValueOf)<0;return!!isInBetween}}}(defaultCompareTypes);defaultCompareTypes=defaultValueOf=global=null;delete defaultCompareTypes;delete defaultValueOf;delete global;return ComparableTrait}.call(null)});
 
-- Simple          -   558 byte
-define(function(){return function(){var i,g=function(f){return function(a){return f.call(a).valueOf()}}(this.Object.prototype.valueOf),h=function(f){return function(a,e,b){var b=typeof b=="function"&&b||f,c,d;return(c=b(a))>(d=b(e))&&1||c<d&&-1||0}}(g),j=function(f){return function(a){a=typeof a=="function"&&a||f;this.compareTo=function(e,b){return a(this,e,b)};this.between=function(e,b,c){var d=a(e,b,c);d<0?d=a(this,e,c)>0&&a(this,b,c)<0:d>0&&(d=a(this,b,c)>0&&a(this,e,c)<0);return!!d}}}(h),h=g=i=null;delete h;delete g;delete i;return j}.call(null)});
+- Simple          -   560 byte
+define(function(){return function(){var i,g=function(f){return function(a){return f.call(a).valueOf()}}(this.Object.prototype.valueOf),h=function(f){return function(a,e,b){var b=typeof b=="function"&&b||f,c,d;return(c=b(a))>(d=b(e))&&1||c<d&&-1||0}}(g),j=function(f){return function(a){a=typeof a=="function"&&a||f;this.compareTo=function(e,b){return a(this,e,b)};this.inBetween=function(e,b,c){var d=a(e,b,c);d<0?d=a(this,e,c)>0&&a(this,b,c)<0:d>0&&(d=a(this,b,c)>0&&a(this,e,c)<0);return!!d}}}(h),h=g=i=null;delete h;delete g;delete i;return j}.call(null)});
 
 
 */
