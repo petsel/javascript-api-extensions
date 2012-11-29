e601e3e38036057b6c2f427ae0358c78e4f6881f
Peter Seliger <peter.seliger@googlemail.com>
2012-11-29 17:36:18 +0100

    After having implemented [[Enumerable]] there are a whole bunch of enumerable trait implementations for both EcmaScript core objects [[Number]] and [[String]] that now can be iterated with [next] and/or [previous] with and without making use of throwing [[StopIteration]].

---
+++ core/Enumerable/Number.next.StopIteration.js
@@ -0,0 +1,86 @@
+(function () {
+
+
+  var
+    global = this,
+
+
+    Num = global.Number,
+/*
+
+    INT_MIN = Math.pow(-2, 53), // (Math.pow(-2, 53) - 1) === (Math.pow(-2, 53)) // true // (Math.pow(-2, 53) + 1) === (Math.pow(-2, 53)) // false
+    INT_MAX = Math.pow(2, 53),  // [http://blog.vjeux.com/2010/javascript/javascript-max_int-number-limits.html]
+                                // via [http://stackoverflow.com/questions/307179/what-is-javascripts-max-int-whats-the-highest-integer-value-a-number-can-go-to#answer-4375743]
+*/
+    parseInt = global.parseInt,
+    isFinite = global.isFinite,
+
+
+    isFunction = (
+
+      (typeof global.isFunction == "function")
+
+      && global.isFunction(global.isFunction)
+      && global.isFunction
+
+    ) || function (obj) {return (typeof obj == "function") && (typeof obj.call == "function") && (typeof obj.apply == "function");},
+
+
+    Enumerable = (isFunction(global.require) && global.require("Enumerable")) || global["Enumerable"]
+  ;
+
+
+  var
+    nextInteger = (function (parse_int, is_finite, UNDEFINED_VALUE) {
+      return function () {
+
+        var
+          currInt = parse_int(this, 10),
+          nextInt = (currInt + 1)
+        ;
+        return ((is_finite(nextInt) && (nextInt !== currInt)) ? nextInt : UNDEFINED_VALUE);
+      };
+    }(parseInt, isFinite)),
+
+
+    compare = function (a, b) {return (a - b);},
+  //compareTo = function (b) {return (this - b);},
+
+
+    EnumerableInteger = Enumerable.create({     // By default this implementation of an enumerable trait generator does not
+                                                // create [Enumerable]s that do stop iterations by throwing [StopIteration].
+      next                  : nextInteger,      // Invalid calls on [next] or [previous] just return [undefined] thus preventing
+    //previous              : previousInteger,  // wrapping each of these calls into rather expensive try...catch statements.
+      compare               : compare,
+
+    //compare               : compareTo,
+      isThrowStopIteration  : true              // if this flag was set to [true] each call on [next] or [previous] needs to be wrapped into a try...catch block.
+    })
+  ;
+
+
+  EnumerableInteger.call(Num.prototype);
+
+
+  return Num;
+
+
+}).call(null);
+
+
+
+/*
+
+
+  [http://closure-compiler.appspot.com/home]
+
+
+- Whitespace only - 809 byte
+(function(){var global=this,Num=global.Number,parseInt=global.parseInt,isFinite=global.isFinite,isFunction=typeof global.isFunction=="function"&&global.isFunction(global.isFunction)&&global.isFunction||function(obj){return typeof obj=="function"&&typeof obj.call=="function"&&typeof obj.apply=="function"},Enumerable=isFunction(global.require)&&global.require("Enumerable")||global["Enumerable"];var nextInteger=function(parse_int,is_finite,UNDEFINED_VALUE){return function(){var currInt=parse_int(this,10),nextInt=currInt+1;return is_finite(nextInt)&&nextInt!==currInt?nextInt:UNDEFINED_VALUE}}(parseInt,isFinite),compare=function(a,b){return a-b},EnumerableInteger=Enumerable.create({next:nextInteger,compare:compare,isThrowStopIteration:true});EnumerableInteger.call(Num.prototype);return Num}).call(null);
+
+
+- Simple          - 483 byte
+(function(){var c=this.Number,d=this.parseInt,e=this.isFinite;(("function"==typeof this.isFunction&&this.isFunction(this.isFunction)&&this.isFunction||function(a){return"function"==typeof a&&"function"==typeof a.call&&"function"==typeof a.apply})(this.require)&&this.require("Enumerable")||this.Enumerable).create({next:function(){var a=d(this,10),b=a+1;return e(b)&&b!==a?b:void 0},compare:function(a,b){return a-b},isThrowStopIteration:!0}).call(c.prototype);return c}).call(null);
+
+
+*/
---
+++ core/Enumerable/String.next.js
@@ -0,0 +1,112 @@
+(function () {
+
+
+  var
+    global = this,
+
+
+    Str = global.String,
+
+
+    fromCharCode = Str.fromCharCode,
+/*
+
+    INT_MIN = Math.pow(-2, 53), // (Math.pow(-2, 53) - 1) === (Math.pow(-2, 53)) // true // (Math.pow(-2, 53) + 1) === (Math.pow(-2, 53)) // false
+    INT_MAX = Math.pow(2, 53),  // [http://blog.vjeux.com/2010/javascript/javascript-max_int-number-limits.html]
+                                // via [http://stackoverflow.com/questions/307179/what-is-javascripts-max-int-whats-the-highest-integer-value-a-number-can-go-to#answer-4375743]
+*/
+    parseInt = global.parseInt,
+    isFinite = global.isFinite,
+
+
+    isFunction = (
+
+      (typeof global.isFunction == "function")
+
+      && global.isFunction(global.isFunction)
+      && global.isFunction
+
+    ) || function (obj) {return (typeof obj == "function") && (typeof obj.call == "function") && (typeof obj.apply == "function");},
+
+
+    Enumerable = (isFunction(global.require) && global.require("Enumerable")) || global["Enumerable"]
+  ;
+
+
+  var
+    nextCharacter = (function (from_char_code, parse_int, is_finite, UNDEFINED_VALUE) {
+      return function () {
+
+        var
+          currVal = ("" + this),
+          nextVal
+        ;
+        if (currVal === "") { // there is nothing like a "next" character that could be a successor of an empty string.
+
+          nextVal = UNDEFINED_VALUE;
+
+        } else if (currVal.length == 1) { // this definitely is a character.
+
+          nextVal = from_char_code(currVal.charCodeAt(0) + 1);
+      /*
+       *  either "nextVal" gets assigned a valid character ...
+       *  ... or assignement of "nextVal" falls back again to the [undefined] value.
+       */
+          nextVal = ((nextVal !== "") && (nextVal !== currVal)) ? nextVal : UNDEFINED_VALUE;
+
+        } else {
+
+          currVal = parse_int(this, 10);
+          nextVal = (currVal + 1);
+      /*
+       *  either "nextVal" gets assigned a valid number ...
+       *  ... or assignement of "nextVal" falls back again to the [undefined] value.
+       */
+          nextVal = (is_finite(nextVal) && (nextVal !== currVal)) ? nextVal : UNDEFINED_VALUE;
+        }
+        return nextVal;
+      };
+    }(fromCharCode, parseInt, isFinite)),
+
+
+    compare = function (a, b) {return (((a < b) && -1) || ((a > b) && 1) || 0);},
+  //compareTo = function (b) {return (((this < b) && -1) || ((this > b) && 1) || 0);},
+
+
+    EnumerableCharacter = Enumerable.create({     // By default this implementation of an enumerable trait generator does not
+                                                  // create [Enumerable]s that do stop iterations by throwing [StopIteration].
+      next                  : nextCharacter,      // Invalid calls on [next] or [previous] just return [undefined] thus preventing
+    //previous              : previousCharacter,  // wrapping each of these calls into rather expensive try...catch statements.
+      compare               : compare
+
+    //compare               : compareTo,
+    //isThrowStopIteration  : true                // if this flag was set to [true] each call on [next] or [previous] needs to be wrapped into a try...catch block.
+    })
+  ;
+
+
+  EnumerableCharacter.call(Str.prototype);
+
+
+  return Str;
+
+
+}).call(null);
+
+
+
+/*
+
+
+  [http://closure-compiler.appspot.com/home]
+
+
+- Whitespace only - 1.088 byte
+(function(){var global=this,Str=global.String,fromCharCode=Str.fromCharCode,parseInt=global.parseInt,isFinite=global.isFinite,isFunction=typeof global.isFunction=="function"&&global.isFunction(global.isFunction)&&global.isFunction||function(obj){return typeof obj=="function"&&typeof obj.call=="function"&&typeof obj.apply=="function"},Enumerable=isFunction(global.require)&&global.require("Enumerable")||global["Enumerable"];var nextCharacter=function(from_char_code,parse_int,is_finite,UNDEFINED_VALUE){return function(){var currVal=""+this,nextVal;if(currVal==="")nextVal=UNDEFINED_VALUE;else if(currVal.length==1){nextVal=from_char_code(currVal.charCodeAt(0)+1);nextVal=nextVal!==""&&nextVal!==currVal?nextVal:UNDEFINED_VALUE}else{currVal=parse_int(this,10);nextVal=currVal+1;nextVal=is_finite(nextVal)&&nextVal!==currVal?nextVal:UNDEFINED_VALUE}return nextVal}}(fromCharCode,parseInt,isFinite),compare=function(a,b){return a<b&&-1||a>b&&1||0},EnumerableCharacter=Enumerable.create({next:nextCharacter,compare:compare});EnumerableCharacter.call(Str.prototype);return Str}).call(null);
+
+
+- Simple          -   587 byte
+(function(){var c=this.String,d=c.fromCharCode,e=this.parseInt,f=this.isFinite;(("function"==typeof this.isFunction&&this.isFunction(this.isFunction)&&this.isFunction||function(b){return"function"==typeof b&&"function"==typeof b.call&&"function"==typeof b.apply})(this.require)&&this.require("Enumerable")||this.Enumerable).create({next:function(){var b=""+this,a;""===b?a=void 0:1==b.length?(a=d(b.charCodeAt(0)+1),a=""!==a&&a!==b?a:void 0):(b=e(this,10),a=b+1,a=f(a)&&a!==b?a:void 0);return a},compare:function(b,a){return b<a&&-1||b>a&&1||0}}).call(c.prototype);return c}).call(null);
+
+
+*/
